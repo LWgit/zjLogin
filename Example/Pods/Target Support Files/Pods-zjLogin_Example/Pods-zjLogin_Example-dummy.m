d513758a4351582b9b047e06662471ca863a9111
lavi.dai <>
2021-07-30 14:33:54 +0800

    init zj

---
+++ Example/Pods/Target Support Files/Pods-zjLogin_Example/Pods-zjLogin_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_zjLogin_Example : NSObject
+@end
+@implementation PodsDummy_Pods_zjLogin_Example
+@end
---
+++ Example/Pods/Target Support Files/zjLogin/zjLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_zjLogin : NSObject
+@end
+@implementation PodsDummy_zjLogin
+@end
